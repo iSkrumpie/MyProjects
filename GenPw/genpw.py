746fa2df120842474a990e7258cf415baa47af3a
Timm Flagmeyer <iskrumpie@gmail.com>
2020-03-01 19:49:23 +0100

    Generate a random password

--- GenPw/genpw.py
+++ GenPw/genpw.py
@@ -1,49 +1,14 @@
 import random
 
-lChars = 'abcdefghijklmnopqrstuvwxyz'
-uChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-digits = '1234567890'
-specialChars = '!@#$%^&*-_+='
+Chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*-_+='
 
 passLen = 26
 myPass = ''
 
 for i in range(passLen):
-    while (len(myPass)) <= 2:
-        index = random.randrange(len(lChars))
-        myPass = myPass + lChars[index]
-        myPassLen = len(myPass)
-    while (len(myPass)) <= 5:
-        index = random.randrange(len(digits))
-        myPass = myPass + digits[index]
-        myPassLen = len(myPass)
-    while (len(myPass)) <= 7:
-        index = random.randrange(len(specialChars))
-        myPass = myPass + specialChars[index]
-        myPassLen = len(myPass)
-    while (len(myPass)) <= 10:
-        index = random.randrange(len(uChars))
-        myPass = myPass + uChars[index]
-        myPassLen = len(myPass)
-    while (len(myPass)) <= 13:
-        index = random.randrange(len(specialChars))
-        myPass = myPass + specialChars[index]
-        myPassLen = len(myPass)
-    while (len(myPass)) <= 16:
-        index = random.randrange(len(digits))
-        myPass = myPass + digits[index]
-        myPassLen = len(myPass)
-    while (len(myPass)) <= 19:
-        index = random.randrange(len(uChars))
-        myPass = myPass + uChars[index]
-        myPassLen = len(myPass)
-    while (len(myPass)) <= 23:
-        index = random.randrange(len(lChars))
-        myPass = myPass + lChars[index]
-        myPassLen = len(myPass)
-    while (len(myPass)) <= 26:
-        index = random.randrange(len(specialChars))
-        myPass = myPass + specialChars[index]
+    while (len(myPass)) <= 25:
+        index = random.randrange(len(Chars))
+        myPass = myPass + Chars[index]
         myPassLen = len(myPass)
 
 print(myPass)
