9ccec3d62cd54dd357ae3eeb8228d7e6e174e2d1
Timm Flagmeyer <iskrumpie@gmail.com>
2020-03-02 17:17:35 +0100

    Scan for open ports at a given host.

--- Portscanner/portscanner.py
+++ Portscanner/portscanner.py
@@ -3,23 +3,16 @@ import subprocess
 import sys
 from datetime import datetime
 
-# Clear the screen
 subprocess.call('clear', shell=True)
 
-# User input
 remoteServer = input("Enter a remote host to scan: ")
 remoteServerIP = socket.gethostbyname(remoteServer)
 
-# Print a little banner
 print("-" * 60)
 print("Please wait, scanning remote host", remoteServerIP)
 print("-" * 60)
 
-# Check what time the scan started
 t1 = datetime.now()
-
-# Using the range function to specify ports (here it will scans all ports between 1 and 1024)
-# We also put in some error handling for catching errors
 
 try:
     for port in range(1, 1025):
@@ -41,11 +34,8 @@ except socket.error:
     print("Couldn't connect to server")
     sys.exit()
 
-# Checking the time again
 t2 = datetime.now()
 
-# Calculates the difference of time, to see how long it took to run the script
 total = t2 - t1
 
-# Printing the information to screen
 print('Scanning Completed in: ', total)
