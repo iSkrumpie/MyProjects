d6d0cf73f718e6862d05a036df19bec84ddb6954
Timm Flagmeyer <iskrumpie@gmail.com>
2020-03-04 12:27:32 +0100

    Generate a random password

--- GenPw/genpw.py
+++ GenPw/genpw.py
@@ -1,17 +1,17 @@
 import random
 
 Chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*-_+='
-
-passlen = input("Please enter the password length: ")
 myPass = ''
+check = 0
 
-try:
-    while (len(myPass)) <= int(passlen) - 1:
-        index = random.randrange(len(Chars))
-        myPass = myPass + Chars[index]
-        myPassLen = len(myPass)
-
-    print(myPass)
-
-except ValueError:
-    print("You have to enter a number, for example: 5")
+while check == 0:
+    passlen = input("Please enter the password length: ")
+    try:
+        while (len(myPass)) <= int(passlen) - 1:
+            index = random.randrange(len(Chars))
+            myPass = myPass + Chars[index]
+            myPassLen = len(myPass)
+        print(myPass)
+        check = 1
+    except ValueError:
+        print("You have to enter a number, for example: 5")
